5bd167efa11adfd9f74549c3abe334706eb26ee8
CriseJ <capt_jacks@163.com>
2018-09-05 20:44:24 +0800

    新添登录注册moudle，将登录和注册模块从主项目分离出来

---
+++ login-regist-moudle/src/main/java/debug/LoginAndRegistApplication.java
@@ -0,0 +1,11 @@
+package debug;
+
+
+import cj_pt.crise.com.base_library.base_application.BaseApplication;
+
+public class LoginAndRegistApplication extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
